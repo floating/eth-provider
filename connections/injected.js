633a289109fe6f53ed37b247a342a51982ead77b
C. Jordan Muir <cjm712@gmail.com>
2018-11-02 03:51:48 -0400

    Update injected error messages

--- connections/injected.js
+++ connections/injected.js
@@ -3,10 +3,10 @@ const EventEmitter = require('events')
 class InjectedConnection extends EventEmitter {
   constructor (_injected, options) {
     super()
-    if (_injected.web3) {
-      setTimeout(() => this.emit('error', new Error('Injected provider via web3 is not supported')), 0)
+    if (_injected) {
+      setTimeout(() => this.emit('error', new Error('Injected web3 provider is not currently supported')), 0)
     } else {
-      setTimeout(() => this.emit('error', new Error('No injected provider not yet available')), 0)
+      setTimeout(() => this.emit('error', new Error('No injected provider found')), 0)
     }
   }
 }
