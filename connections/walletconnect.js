2a9600cb9564c9eca80f4dbce945236f966844d5
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-28 13:47:35 +0200

    add networkChanged event to walletconnect

--- connections/walletconnect.js
+++ connections/walletconnect.js
@@ -4,7 +4,7 @@ const dev = process.env.NODE_ENV === 'development'
 let WalletConnect, WCQRCode
 
 class WalletConnectConnection extends EventEmitter {
-  constructor (_WalletConnect, _WCQRCode, options) {
+  constructor(_WalletConnect, _WCQRCode, options) {
     super()
     WalletConnect = _WalletConnect
     WCQRCode = _WCQRCode
@@ -13,12 +13,12 @@ class WalletConnectConnection extends EventEmitter {
     this.on('error', () => this.close())
     setTimeout(() => this.create(options), 0)
   }
-  openQR () {
+  openQR() {
     WCQRCode.open(this.wc.uri, () => {
       this.emit('error', new Error('User close WalletConnect QR Code modal'))
     })
   }
-  create (options) {
+  create(options) {
     if (!WalletConnect) this.emit('error', new Error('WalletConnect not available'))
 
     try {
@@ -43,6 +43,8 @@ class WalletConnectConnection extends EventEmitter {
       this.accounts = accounts
       this.chainId = chainId
 
+      this.updateNetworkId(chainId)
+
       // Emit connect event
       this.emit('connect')
     })
@@ -61,33 +63,44 @@ class WalletConnectConnection extends EventEmitter {
       // Check if chainId changed and trigger event
       if (this.chainId !== chainId) {
         this.chainId = chainId
-        this.emit('networkChanged', chainId)
+        this.emit('chainChanged', chainId)
       }
+
+      // Check if networkId changed and trigger event
+      this.updateNetworkId(chainId)
+
     })
     this.wc.on('disconnect', (e, payload) => {
       if (e) return this.emit('error', e)
       this.onClose()
     })
   }
-  onClose () {
+  onClose() {
     this.wc = null
     this.closed = true
     if (dev) console.log('Closing WalletConnector connection')
     this.emit('close')
     this.removeAllListeners()
   }
-  close () {
+  close() {
     if (this.wc) return this.wc.killSession()
     this.onClose()
   }
-  error (payload, message, code = -1) {
+  error(payload, message, code = -1) {
     this.emit('payload', { id: payload.id, jsonrpc: payload.jsonrpc, error: { message, code } })
   }
-  send (payload) {
+  send(payload) {
     if (this.wc && this.wc.connected) {
       return this.wc.sendCustomRequest(payload)
     } else {
       return this.error(payload, 'Not connected')
+    }
+  }
+  async updateNetworkId(chainId) {
+    const networkId = convertChainIdToNetworkId(chainId)
+    if (this.networkId !== networkId) {
+      this.networkId = networkId
+      this.emit('networkChanged', networkId)
     }
   }
 }
