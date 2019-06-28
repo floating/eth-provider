6e8885511133602235f49abf6901cd1685dc8674
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-28 14:41:32 +0200

    handle read calls with http connection and add network changed

--- connections/walletconnect.js
+++ connections/walletconnect.js
@@ -1,7 +1,12 @@
+const { getChain } = require('evm-chains')
+const { convertNumberToHex } = require('@walletconnect/browser')
 const EventEmitter = require('events')
+const presets = require('../presets')
 const dev = process.env.NODE_ENV === 'development'
 
 let WalletConnect, WCQRCode
+
+const XHR = typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined' ? window.XMLHttpRequest : null
 
 class WalletConnectConnection extends EventEmitter {
   constructor(_WalletConnect, _WCQRCode, options) {
@@ -10,6 +15,7 @@ class WalletConnectConnection extends EventEmitter {
     WCQRCode = _WCQRCode
     this.bridge = options.walletConnectBridge
     this.qrcode = options.walletConnectQR
+    this.infuraId = options.walletConnectInfuraId
     this.on('error', () => this.close())
     setTimeout(() => this.create(options), 0)
   }
@@ -39,11 +45,11 @@ class WalletConnectConnection extends EventEmitter {
       if (this.qrcode) WCQRCode.close() // Close QR Code Modal
       const { accounts, chainId } = payload.params[0] // Get provided accounts and chainId
 
-      // Save accounts and chainId
+      // Save accounts
       this.accounts = accounts
-      this.chainId = chainId
 
-      this.updateNetworkId(chainId)
+      // Handle chain update
+      this.updateChain(chainId)
 
       // Emit connect event
       this.emit('connect')
@@ -60,14 +66,8 @@ class WalletConnectConnection extends EventEmitter {
         this.emit('accountsChanged', accounts)
       }
 
-      // Check if chainId changed and trigger event
-      if (this.chainId !== chainId) {
-        this.chainId = chainId
-        this.emit('chainChanged', chainId)
-      }
-
-      // Check if networkId changed and trigger event
-      this.updateNetworkId(chainId)
+      // Handle chain update
+      this.updateChain(chainId)
 
     })
     this.wc.on('disconnect', (e, payload) => {
@@ -89,18 +89,121 @@ class WalletConnectConnection extends EventEmitter {
   error(payload, message, code = -1) {
     this.emit('payload', { id: payload.id, jsonrpc: payload.jsonrpc, error: { message, code } })
   }
-  send(payload) {
+  async send(payload) {
+    const signingMethods = [
+      'eth_sendTransaction',
+      'eth_signTransction',
+      'eth_sign',
+      'eth_signTypedData',
+      'eth_signTypedData_v1',
+      'eth_signTypedData_v3',
+      'personal_sign'
+    ]
+    const stateMethods = [
+      'eth_accounts',
+      'eth_chainId',
+      'net_version'
+    ]
     if (this.wc && this.wc.connected) {
-      return this.wc.sendCustomRequest(payload)
+      if (signingMethods.includes(payload.method)) {
+        return await this.wc.sendCustomRequest(payload)
+      } else if (stateMethods.includes(payload.method)) {
+        switch (payload.method) {
+          case 'eth_accounts':
+            return {
+              id: payload.id,
+              jsonrpc: payload.jsonrpc,
+              result: this.accounts
+            }
+          case 'eth_chainId':
+            return {
+              id: payload.id,
+              jsonrpc: payload.jsonrpc,
+              result: convertNumberToHex(this.chainId)
+            }
+
+          case 'net_version':
+            return {
+              id: payload.id,
+              jsonrpc: payload.jsonrpc,
+              result: this.networkId
+            }
+          default:
+            break;
+        }
+      } else {
+        return await this.httpConnection.send(payload)
+      }
     } else {
       return this.error(payload, 'Not connected')
     }
   }
-  async updateNetworkId(chainId) {
-    const networkId = convertChainIdToNetworkId(chainId)
+
+  async updateChain(chainId) {
+    if (this.chainId === chainId) {
+      return;
+    }
+    const chain = await getChain(chainId)
+
+    // Check if chainId changed and trigger event
+    if (this.chainId !== chainId) {
+      this.chainId = chainId
+      this.emit('chainChanged', chainId)
+    }
+
+    const { networkId } = chain
+    // Check if networkId changed and trigger event
     if (this.networkId !== networkId) {
       this.networkId = networkId
       this.emit('networkChanged', networkId)
+    }
+
+    // Handle rpcUrl update
+    this.updateRpcUrl(chain)
+  }
+
+  updateRpcUrl(chain) {
+    const { chainId, rpc } = chain
+    if (rpc.length) {
+      if (this.infuraId) {
+        const matches = rpc.filter(rpcUrl => rpcUrl.inclues('infura.io'))
+        if (matches && matches.length) {
+          this.rpcUrl = matches[0].replace("${INFURA_API_KEY}", this.infuraId)
+        } else {
+          this.rpcUrl = rpc[0]
+        }
+      }
+    } else {
+      this.rpcUrl = this.getPresetRpcUrl(chainId)
+    }
+    // Handle httpConnection update
+    this.updateHttpConnection()
+  }
+
+  updateHttpConnection = (options) => {
+    if (this.rpcUrl) {
+      this.httpConnection = new HTTPConnection(XHR, this.rpcUrl, options)
+    }
+  }
+
+  getPresetRpcUrl(chainId) {
+    switch (chainId) {
+      case 1:
+        return presets.infura[1]
+      case 3:
+        return presets.infuraRopsten[1]
+
+      case 4:
+        return presets.infuraRinkeby[1]
+
+      case 5:
+        return presets.infuraGoerli[1]
+
+      case 42:
+        return presets.infuraKovan[1]
+      default:
+        return ''
+
     }
   }
 }
