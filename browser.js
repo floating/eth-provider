48b0760ac2f069342994ba6618cfccd0e4560f00
Matt Holtzman <matt.holtzman@gmail.com>
2024-08-02 20:50:52 -0400

    use global WebSocket if none available on window (#74)

--- browser.js
+++ browser.js
@@ -6,7 +6,19 @@ const injected = {
   ethereum: typeof window !== 'undefined' && typeof window.ethereum !== 'undefined' ? window.ethereum : null,
   web3: typeof window !== 'undefined' && typeof window.web3 !== 'undefined' ? window.web3.currentProvider : null
 }
-const ws = typeof window !== 'undefined' && typeof window.WebSocket !== 'undefined' ? window.WebSocket : null
+
+const ws = () => {
+  if (typeof window !== 'undefined' && typeof window.WebSocket !== 'undefined') {
+    return window.WebSocket
+  }
+
+  if (typeof WebSocket !== 'undefined') {
+    return WebSocket
+  }
+
+  return null
+}
+
 const XHR = typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined' ? window.XMLHttpRequest : null
 
 if (injected.ethereum) injected.ethereum.__isProvider = true
@@ -14,7 +26,7 @@ if (injected.ethereum) injected.ethereum.__isProvider = true
 const connections = {
   injected: injected.ethereum || require('./connections/injected')(injected.web3),
   ipc: require('./connections/unavailable')('IPC connections are unavliable in the browser'),
-  ws: require('./connections/ws')(ws),
+  ws: require('./connections/ws')(ws()),
   http: require('./connections/http')(XHR)
 }
 
